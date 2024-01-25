dbc0db5344dce1f8594bea01e7886d45aa5bf9da
Hamza <149156223+Hamza-Foughali@users.noreply.github.com>
2024-01-25 22:30:15 +0100

    Update clothing.js

--- js/clothing.js
+++ js/clothing.js
@@ -1,7 +1,4 @@
 function performLogin() {
-    // You may want to include actual login logic here
-
-    // For demonstration purposes, let's assume the login is successful
-    // and redirect to the dashboard page
+    
     window.location.href = "clothing2.html";
 }
